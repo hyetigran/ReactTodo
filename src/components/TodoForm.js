35a4990c9e3bf1e2f6d96fd69ac29eccac335d82
Tigran Asriyan <tigran.asriyan@gmail.com>
2021-05-24 15:10:06 +0400

    created todo form jsx

---
+++ src/components/TodoForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./TodoForm.css"
+
+function TodoForm() {
+
+    return (
+        <div>
+            <form>
+                <p>Add Todo Form</p>
+                <input />
+                <button>Create Todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default TodoForm;
